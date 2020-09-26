1a607d4440c48dc80ec49d0a98193e66a1fa034f
Ruben Paz <rubenpazchuspe@outlook.com>
2020-09-26 12:07:04 -0500

    add bootstrap into the project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import '../css/App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Button>testsss</Button>
     </div>
   );
 }
