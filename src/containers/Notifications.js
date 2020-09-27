7968c2268d13e14e7e87fd016e56f20d7bc6e417
rubenpazch <rubenpazchuspe@outlook.com>
2020-09-27 17:13:20 -0500

    add notification sub component on notification container

--- src/containers/Notifications.js
+++ src/containers/Notifications.js
@@ -3,12 +3,13 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import styles from '../css/notifications.module.css';
+import MenuNotificationBar from '../components/MenuNotificationBar';
 
 function Notifications() {
   return (
     <Row className={styles.wrapper}>
       <Container className={styles.contentWrapper}>
-        <p>Notifications</p>
+        <MenuNotificationBar/>
       </Container>
     </Row>
   );
