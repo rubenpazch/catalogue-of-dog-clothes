669b6c8a7ae70a55c0d4f3b1972edf908ef20d2b
rubenpazch <rubenpazchuspe@outlook.com>
2020-09-27 02:05:59 -0500

    add navbar into the header page

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 
-import './App.scss';
+import styles from './app.module.css';
 import NavBar from './containers/NavBar';
 import Notifications from './containers/Notifications';
+import Slider from './containers/Slider';
 
 function App() {
   return (
-    <Container fluid className="wrapperApp">
+    <Container fluid className={styles.mainWrapper}>
       <Notifications />
-      <Container className="wrapperMidle">
-        <h1>title</h1>
+      <NavBar />
+      <Container className={styles.contentWrapper}>
+        <Slider />
+        <h1>titleasdasd</h1>
       </Container>
     </Container>
   );
