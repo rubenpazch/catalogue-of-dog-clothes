56cd36db0cfe724c56b69e7d1f336c025c63cd08
rubenpazch <rubenpazchuspe@outlook.com>
2020-09-27 12:48:29 -0500

    add logo into navigation bar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import styles from '../css/navbar.module.css';
+import logoImg from '../img/logo.png';
 
 function Navbar() {
   return (
     <Row className={styles.wrapper}>
       <Container className={styles.contentWrapper}>
-        <p>Navbar</p>
+        <div class={styles.logo}>
+					<img src={logoImg}/>
+				</div>
       </Container>
     </Row>
   );
