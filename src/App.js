befc448754fc01a419a350b49a077abb4361141c
rubenpazch <rubenpazchuspe@outlook.com>
2020-09-27 22:23:26 -0500

    add feature tours into the main page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
-
 import styles from './app.module.css';
 import NavBar from './containers/NavBar';
 import Notifications from './containers/Notifications';
 import Slider from './containers/Slider';
+import FeatureTours from './containers/FeatureTours';
 
 function App() {
   return (
@@ -15,8 +15,7 @@ function App() {
       <NavBar />
       <Slider />
       <Container className={styles.contentWrapper}>
-
-        <h1>titleasdasd</h1>
+        <FeatureTours/>
       </Container>
     </Container>
   );
