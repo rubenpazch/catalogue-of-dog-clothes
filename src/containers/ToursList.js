b086f55ff47fcbc38a4547a71ccd221158be405d
Ruben Paz <rubenpazchuspe@outlook.com>
2020-09-29 01:13:01 -0500

    add item tour for home page

--- src/containers/ToursList.js
+++ src/containers/ToursList.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import styles from '../css/tourslist.module.css';
-import ItemFeatureTour from '../components/ItemFeatureTour';
+import ItemTour from '../components/ItemTour';
 
 function ToursList() {
   return (
     <Row className={styles.wrapper}>
-      <Col></Col>
-      <Col></Col>
-      <Col></Col>
+      <Col md={9} className={styles.wrapperContent}>
+        <Row className={styles.wrapperTours}>
+          <ItemTour />
+          <ItemTour />
+          <ItemTour />
+          <ItemTour />
+          <ItemTour />
+          <ItemTour />
+          <ItemTour />
+          <ItemTour />
+          <ItemTour />
+        </Row>
+      </Col>
+      <Col md={3} className={styles.filters}>
+        <p>filters</p>
+      </Col>
     </Row>
   );
 }
