69bbe2736a238624ab4099f84042ed35f9eddcb9
Ruben Paz <rubenpazchuspe@outlook.com>
2020-09-29 13:17:00 -0500

    add initial state for tours and filter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
+import rootReducer from './reducers/index';
+
+const deafultTours = [
+  { title: 'Tour 1' },
+  { title: 'Tour 2' },
+];
+
+const deafaultFilter = 'All';
+
+const store = createStore(
+  rootReducer({
+    tours: deafultTours,
+    filter: deafaultFilter,
+  }),
+  composeWithDevTools(),
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
