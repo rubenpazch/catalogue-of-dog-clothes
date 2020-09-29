a8c0698161a132b9af06b23d3863df1645189d6f
Ruben Paz <rubenpazchuspe@outlook.com>
2020-09-29 01:12:25 -0500

    component tour

--- src/components/ItemTour.js
+++ src/components/ItemTour.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Container from 'react-bootstrap/Container';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import styles from '../css/itemtour.module.css';
+import img1 from '../img/tour1.jpg';
 
-const ItemTour = ({ image, text }) => (
-  <Row className={styles.wrapper}>
-    <img src={image} />
-    <p>{text}</p>
-  </Row>
+const ItemTour = () => (
+  <Col className={styles.wrapper}>
+    <img src={img1} alt="tour1" />
+    <div className={styles.header}>
+      <h5 md={9}>City Tour Cusco 1D</h5>
+      <span md={3}>$15.00</span>
+    </div>
+    <div className={styles.services}>
+      <ul className={styles.listServices}>
+        <li>
+          <FontAwesomeIcon icon={faCoffee} />
+          Guiding
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faCoffee} />
+          Transportation
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faCoffee} />
+          Entrances
+        </li>
+        <li>
+          <FontAwesomeIcon icon={faCoffee} />
+          English
+        </li>
+      </ul>
+      <div>
+        text
+      </div>
+    </div>
+
+  </Col>
 );
 
 export default ItemTour;
