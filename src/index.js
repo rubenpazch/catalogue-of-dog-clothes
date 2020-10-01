bf9108c9b5a7019f055663bd8bd62874bde1818f
Ruben Paz <rubenpazchuspe@outlook.com>
2020-09-30 22:23:58 -0500

    add middle ware to user services

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import App from './App';
 import rootReducer from './reducers/index';
+import { fetchTours } from './actions/index';
 
 const deafultTours = [
   { title: 'Tour 1' },
   { title: 'Tour 2' },
+  { title: 'Tour 3' },
+  { title: 'Tour 4' },
+  { title: 'Tour 5' },
+  { title: 'Tour 6' },
 ];
+
+const initialState = {
+  loading: false,
+  tours: [],
+  error: '',
+};
 
 const deafaultFilter = 'All';
 
 const store = createStore(
   rootReducer({
-    tours: deafultTours,
+    tours: initialState,
     filter: deafaultFilter,
   }),
-  composeWithDevTools(),
+  compose(
+    composeWithDevTools(),
+    applyMiddleware(thunk),
+  ),
 );
+//store.dispatch(fetchTours());
 
 ReactDOM.render(
   <Provider store={store}>
