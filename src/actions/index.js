decbfe8cbe91fbbc82f2ff70de3f6f8422989d3a
Ruben Paz <rubenpazchuspe@outlook.com>
2020-09-30 22:24:39 -0500

    create actions methos to get data from the service

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as ActionTypes from '../constants/action-types';
 
 const createTour = tour => ({
@@ -10,7 +11,40 @@ const filterChange = cityCode => ({
   payload: cityCode,
 });
 
+const fetchToursRequest = () => ({
+  type: ActionTypes.FETCH_TOURS_REQUEST,
+});
+
+const fetchToursSuccess = tours => ({
+  type: ActionTypes.FETCH_TOURS_SUCCESS,
+  payload: tours,
+});
+
+const fetchToursFailure = error => ({
+  type: ActionTypes.FETCH_TOURS_FAILURE,
+  payload: error,
+});
+
+const fetchTours = () => async dispatch => {
+  try {
+    const res = await axios.get('http://jsonplaceholder.typicode.com/users');
+    dispatch({
+      type: ActionTypes.FETCH_TOURS_SUCCESS,
+      payload: res.data,
+    });
+  } catch (e) {
+    dispatch({
+      type: ActionTypes.FETCH_TOURS_FAILURE,
+      payload: console.log(e),
+    });
+  }
+};
+
 export {
   createTour,
   filterChange,
+  fetchToursRequest,
+  fetchToursSuccess,
+  fetchToursFailure,
+  fetchTours,
 };
