4838c7b21c18f41bd087f6f0527aa37689a3aac0
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-05 17:13:11 -0500

    delete dispatch and add suscribe to show logs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { composeWithDevTools } from 'redux-devtools-extension';
+//import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import App from './App';
 import rootReducer from './reducers/index';
-import fetchTours from './actions/GetTours';
+
 
 // const deafultTours = [
 //  { title: 'Tour 1' },
@@ -24,16 +24,6 @@ const store = createStore(
   rootReducer,
   applyMiddleware(thunk),
 );
-store.dispatch(fetchTours());
-store.subscribe(() => {
-  const state = store.getState();
-  if (state.status === 'waiting') {
-    console.log('Loading');
-  }
-  if (state.status === 'received') {
-    console.log(state.data[0]);
-  }
-});
 
 ReactDOM.render(
   <Provider store={store}>
