fd567af64c58673e2caedf3a579bb2545247cb1d
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-07 22:23:15 -0500

    add token reducer to get the token

--- src/redux/reducers/root.reducer.js
+++ src/redux/reducers/root.reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import incrementReducer from './increment.reducer';
 import mealsReducer from './meals.reducer';
+import tokenReducer from './token.reducer';
 
 export default combineReducers({
   incrementStore: incrementReducer,
   mealsStore: mealsReducer,
+  tokenStore: tokenReducer,
 });
