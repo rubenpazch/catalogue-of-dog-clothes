071a8cda968f4745e3d2c7c3ee3de62d9c4e3d46
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-13 21:57:00 -0500

    dele not used file from the project

--- src/redux/actions/meals.actions.js
+++
@@ -1,8 +0,0 @@
-import * as actionsTypes from '../actionsType';
-
-const getMealsAsync = result => ({
-  type: actionsTypes.FETCH_MEALS_SUCCESS,
-  meals: result,
-});
-
-export default getMealsAsync;
