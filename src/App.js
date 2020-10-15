7c64e38db5956004ed3cbdafc1615d177dcee393
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-14 22:59:00 -0500

    add route for navigating from tour list to tour detail

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import Container from 'react-bootstrap/Container';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import styles from './app.module.css';
 import NavBar from './containers/NavBar';
 import Notifications from './containers/Notifications';
@@ -9,23 +14,34 @@ import FeatureTours from './containers/FeatureTours';
 import Navigation from './containers/Navigation';
 import ToursList from './containers/ToursList';
 import getTokenAsync from './redux/services/token.service';
+import TourDetail from './components/TourDetail';
 
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getTokenAsync());
   }, []);
+
   return (
-    <Container fluid className={styles.mainWrapper}>
-      <Notifications />
-      <NavBar />
-      <Slider />
-      <Container className={styles.contentWrapper}>
-        <FeatureTours />
-        <Navigation />
-        <ToursList />
+    <Router>
+      <Container fluid className={styles.mainWrapper}>
+        <Notifications />
+        <NavBar />
+        <Slider />
+        <Container className={styles.contentWrapper}>
+          <Switch>
+            <Route path="/tourdetail/:id">
+              <TourDetail />
+            </Route>
+            <Route path="/">
+              <FeatureTours />
+              <Navigation />
+              <ToursList />
+            </Route>
+          </Switch>
+        </Container>
       </Container>
-    </Container>
+    </Router>
   );
 }
 
