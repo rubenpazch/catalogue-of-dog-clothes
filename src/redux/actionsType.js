5412b85d42e89b98e047caf26a332c5591cab676
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-14 23:00:39 -0500

    delete not used actions types

--- src/redux/actionsType.js
+++ src/redux/actionsType.js
@@ -4,15 +4,6 @@ export const FETCH_TOURS_REQUEST = 'FETCH_TOURS_REQUEST';
 export const FETCH_TOURS_SUCCESS = 'FETCH_TOURS_SUCCESS';
 export const FETCH_TOURS_FAILURE = 'FETCH_TOURS_FAILURE';
 
-/* for testing */
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const LOADING = 'LOADING';
-
-/* for meals */
-export const FETCH_MEALS_REQUEST = 'FETCH_MEALS_REQUEST';
-export const FETCH_MEALS_SUCCESS = 'FETCH_MEALS_SUCCESS';
-
 /* for tokens */
 export const FETCH_TOKEN_REQUEST = 'FETCH_TOKEN_REQUEST';
 export const FETCH_TOKEN_SUCCESS = 'FETCH_TOKEN_SUCCESS';
